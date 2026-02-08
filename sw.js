e4f99cc75ce556a2ce2a3ccc9e0549182e67569d
GrieezBtc <muhammadjamiuyusuf876@gmail.com>
2026-02-08 02:10:18 +0100

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pro-calc-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets from Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
